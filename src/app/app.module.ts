e3adb2bf4a945a8457c0cbf8e81a1a9dcf2b3341
JCmontero22 <johan00712@gmail.com>
2023-11-09 08:29:27 -0500

    Actulizacion de codigo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from "@angular/common/http";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DragonBallzModule } from './dragon-ballz/dragon-ballz.module';
@@ -13,7 +15,8 @@ import { DragonBallzModule } from './dragon-ballz/dragon-ballz.module';
   imports: [
     BrowserModule,
     DragonBallzModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
